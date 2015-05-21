c2b3dbed1b1ed04582bc609b7710d7d24d0d4b3c
Cooper Maruyama <cooperm@susteen.com>
2015-05-21 07:41:06 -0700

    Coffee aliases.

--- custom/coffeescript.zsh
+++ custom/coffeescript.zsh
@@ -9,3 +9,18 @@ cmp() {
 
 alias cm=coffeeMe
 
+
+# compile a string of coffeescript and print to output
+cf () {
+  coffee -peb $1
+}
+# compile & copy to clipboard
+cfc () {
+  cf $1 | pbcopy
+}
+
+# compile from pasteboard & print
+alias cfp='coffeeMe "$(pbpaste)"'
+
+# compile from pasteboard and copy to clipboard
+alias cfpc='cfp | pbcopy'
